4f672eaf677b3ad8ab310235fc559875dd3b03ef
MURAOKA Taro <koron.kaoriya@gmail.com>
2022-11-26 16:30:56 +0900

    WIP: ENCODING support

--- vcread/vcread.go
+++ vcread/vcread.go
@@ -9,6 +9,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"strings"
 )
 
 type Type int
@@ -19,11 +20,29 @@ const (
 	ValueType
 )
 
+type mode int
+
+const (
+	nameMode mode = iota
+	paramMode
+	valueMode
+)
+
+type encoding int
+
+const (
+	rawEnc encoding = iota
+	qpEnc
+	b64Enc
+)
+
 type Reader struct {
 	br *bufio.Reader
 
-	mode Type
-	pend []byte
+	mode mode
+
+	paramData []byte
+	encoding  encoding
 }
 
 func New(r io.Reader) *Reader {
@@ -33,7 +52,7 @@ func New(r io.Reader) *Reader {
 	}
 	return &Reader{
 		br:   br,
-		mode: NameType,
+		mode: nameMode,
 	}
 }
 
@@ -73,11 +92,11 @@ var _ Token = (*ValueToken)(nil)
 
 func (r *Reader) Read() (Token, error) {
 	switch r.mode {
-	case NameType:
+	case nameMode:
 		return r.readName()
-	case ParamType:
+	case paramMode:
 		return r.readParam()
-	case ValueType:
+	case valueMode:
 		return r.readValue()
 	}
 	return nil, fmt.Errorf("unknown parse mode: %d", r.mode)
@@ -100,29 +119,32 @@ func (r *Reader) readName() (Token, error) {
 	x := bytes.IndexByte(b, ';')
 	// no parameters
 	if x == -1 {
-		r.mode = ValueType
+		r.mode = valueMode
+		r.paramData = nil
+		r.encoding = rawEnc
 		return &NameToken{NameBytes: b}, nil
 	}
 
 	bn, bp := b[:x], b[x+1:]
-	r.mode = ParamType
-	r.pend = bp
+	r.mode = paramMode
+	r.paramData = bp
+	r.encoding = rawEnc
 	return &NameToken{NameBytes: bn}, nil
 }
 
 func (r *Reader) readParam() (Token, error) {
 	var curr []byte
 	// find delimiter ';' for next parameters
-	x := bytes.IndexByte(r.pend, ';')
+	x := bytes.IndexByte(r.paramData, ';')
 	if x != -1 {
 		// has a delimiter ';' for next parameters
-		curr = r.pend[:x]
-		r.pend = r.pend[x+1:]
+		curr = r.paramData[:x]
+		r.paramData = r.paramData[x+1:]
 	} else {
 		// no more parameters
-		curr = r.pend
-		r.pend = nil
-		r.mode = ValueType
+		curr = r.paramData
+		r.paramData = nil
+		r.mode = valueMode
 	}
 
 	y := bytes.IndexByte(curr, '=')
@@ -131,16 +153,42 @@ func (r *Reader) readParam() (Token, error) {
 		return &ParamToken{NameBytes: curr}, nil
 	}
 	name, value := curr[:y], curr[y+1:]
-	// FIXME: parse special cases (ex. ENCODING=QUOTED-PRINTABLE)
+	// parse special cases (ex. ENCODING=QUOTED-PRINTABLE)
+	if strings.ToUpper(string(name)) == "ENCODING" {
+		encname := string(value)
+		switch strings.ToUpper(encname) {
+		case "7BIT", "8BIT":
+			r.encoding = rawEnc
+		case "QUOTED-PRINTABLE":
+			r.encoding = qpEnc
+		case "B", "BASE64":
+			r.encoding = b64Enc
+		default:
+			return nil, fmt.Errorf("unknown encoding: %s", encname)
+		}
+	}
 	// parameter with a value
 	return &ParamToken{NameBytes: name, ValueBytes: value}, nil
 }
 
 func (r *Reader) readValue() (Token, error) {
+	switch r.encoding {
+	case rawEnc:
+		return r.readValueRaw()
+	case qpEnc:
+		return r.readValueQP()
+	case b64Enc:
+		return r.readValueB64()
+	default:
+		return nil, fmt.Errorf("unknown encoding: %v", r.encoding)
+	}
+}
+
+func (r *Reader) readValueRaw() (Token, error) {
 	b, err := r.br.ReadBytes('\n')
 	if err != nil {
 		if errors.Is(err, io.EOF) {
-			r.mode = NameType
+			r.mode = nameMode
 			return &ValueToken{
 				ValueBytes: b,
 				Continue:   false,
@@ -152,7 +200,7 @@ func (r *Reader) readValue() (Token, error) {
 	next, err := r.br.ReadByte()
 	if err != nil {
 		if errors.Is(err, io.EOF) {
-			r.mode = NameType
+			r.mode = nameMode
 			return &ValueToken{
 				ValueBytes: b,
 				Continue:   false,
@@ -169,9 +217,19 @@ func (r *Reader) readValue() (Token, error) {
 	}
 	// no next line.
 	r.br.UnreadByte()
-	r.mode = NameType
+	r.mode = nameMode
 	return &ValueToken{
 		ValueBytes: b,
 		Continue:   false,
 	}, nil
+}
+
+func (r *Reader) readValueQP() (Token, error) {
+	// TODO: read QUOTED-PRINTABLE encoded value.
+	return r.readValueRaw()
+}
+
+func (r *Reader) readValueB64() (Token, error) {
+	// TODO: read BASE64 encoded value.
+	return r.readValueRaw()
 }
